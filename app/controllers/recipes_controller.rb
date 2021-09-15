05040b71b710c55eddd830158d2e59cef15fcd09
hiddencilantro <stevekim1109@gmail.com>
2021-09-15 16:50:54 -0400

    create, update, destroy actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,4 +8,35 @@ class RecipesController < ApplicationController
         recipe = Recipe.find_by(id: params[:id])
         render json: RecipeSerializer.new(recipe)
     end
+
+    def create
+        recipe = Recipe.new(recipe_params)
+        recipe.category = Category.last ##temporary workaround
+        if recipe.save
+            render json: RecipeSerializer.new(recipe)
+        else
+            render json: {error: "Could not save recipe"}
+        end
+    end
+
+    def update
+        recipe = Recipe.find_by(id: params[:id])
+        if recipe.update(recipe_params)
+            render json: RecipeSerializer.new(recipe)
+        else
+            render json: {error: "Could not update recipe to the database"}
+        end
+    end
+
+    def destroy
+        recipe = Recipe.find_by(id: params[:id])
+        recipe.destroy
+        render json: {message: "Successfully deleted #{recipe.name}"}
+    end
+
+    private
+
+    def recipe_params
+        params.require(:recipe).permit(:name, :description, :image, ingredients: [], instructions: [])
+    end
 end
