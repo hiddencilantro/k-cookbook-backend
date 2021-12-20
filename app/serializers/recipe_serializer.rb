6e39443f71b4067eaa4e4039707d791ef80f8aba
hiddencilantro <stevekim1109@gmail.com>
2021-12-20 15:57:49 -0500

    removed category

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,4 +1,4 @@
 class RecipeSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :description, :image, :ingredients, :instructions, :category_id, :category
+  attributes :name, :description, :image, :ingredients, :instructions, :category_id
 end
