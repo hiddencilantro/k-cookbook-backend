5d164cd0626d76cf1db745ac16fce724793108e7
hiddencilantro <stevekim1109@gmail.com>
2022-01-14 14:34:27 -0500

    category_name= method (for adding own category), titleize names before save

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,20 @@
 class Recipe < ApplicationRecord
 	belongs_to :category
 	validates :name, :ingredients, :instructions, presence: true
+	before_save :titleize_names
+
+	def category_name=(name)
+		if !name.blank?
+			self.category = Category.find_or_create_by(name: name.capitalize)
+		end
+	end
+
+	private
+
+	def titleize_names
+		self.name = name.titleize
+		if !eng_name.blank?
+			self.eng_name = eng_name.titleize
+		end
+	end
 end
