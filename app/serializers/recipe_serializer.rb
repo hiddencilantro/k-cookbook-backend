1cbdf135d60fa876a2d1b39fef009022c6a02bf0
hiddencilantro <stevekim1109@gmail.com>
2022-01-14 14:35:46 -0500

    added has_many/belongs_to to serializers

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,4 +1,5 @@
 class RecipeSerializer
   include FastJsonapi::ObjectSerializer
+  belongs_to :category
   attributes :name, :eng_name, :description, :image, :ingredients, :instructions, :category_id
 end
