8c55612b3004bcd5c47ae2154644118d602be2f3
hiddencilantro <stevekim1109@gmail.com>
2022-02-16 15:04:03 -0500

    updated cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,6 +14,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get]
     resource '/recipes',
       headers: :any,
-      methods: [:post, :patch, :delete]
+      methods: [:post]
+    resource '/recipes/*',
+      headers: :any,
+      methods: [:patch, :delete]
   end
 end
